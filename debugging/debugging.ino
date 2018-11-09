5859532f0560143643a44a8c1bbf307622cd3a77
Joppe Koers <31915428+SirMorfield@users.noreply.github.com>
2018-11-09 20:52:27 +0100

    IT WURKS

--- debugging/debugging.ino
+++ debugging/debugging.ino
@@ -1,40 +1,6 @@
-//int clockPin = A0; //SH_CP
-//int latchPin = A1; //ST_CP
-//int dataPin = A2; //DS
-//
-//void setup() {
-//  Serial.begin(115200);
-//  pinMode(latchPin, OUTPUT);
-//  pinMode(clockPin, OUTPUT);
-//  pinMode(dataPin, OUTPUT);
-//}
-//
-//void loop() {
-//  digitalWrite(latchPin, HIGH);
-//
-//  for (int x = 0; x < 48; x++) {
-//    digitalWrite(clockPin, LOW);
-//    digitalWrite(dataPin, HIGH); //the money
-//    digitalWrite(clockPin, HIGH);
-//    digitalWrite(dataPin, LOW);
-//
-//  }
-//
-//  for (int x = 0; x < 8; x++) {
-//    digitalWrite(clockPin, LOW);
-//    digitalWrite(dataPin, LOW); //the money
-//    digitalWrite(clockPin, HIGH);
-//    digitalWrite(dataPin, LOW);
-//  }
-//
-//  digitalWrite(latchPin, LOW);
-//}
-
-int clockPin = A0; //SH_CP
-int latchPin = A1; //ST_CP
-int dataPin = A2; //DS
-int invertedBitPos[8] = {0b01111111, 0b10111111, 0b11011111, 0b11101111, 0b11110111, 0b11111011, 0b11111101, 0b11111110};
-
+#define clockPin A0 //SH_CP
+#define latchPin A1 //ST_CP
+#define dataPin A2 //DS
 
 void setup() {
   Serial.begin(115200);
@@ -50,10 +16,47 @@ void loop() {
   for (int y = 0; y <  8; y++) {
     digitalWrite(latchPin, HIGH);
     for (int x = 0; x < 48; x++) {
-      digitalWrite(clockPin, LOW);
-      digitalWrite(dataPin, HIGH); //the money
-      digitalWrite(clockPin, HIGH);
-      digitalWrite(dataPin, LOW);
+if (x == 2) {
+        digitalWrite(clockPin, LOW);
+        digitalWrite(dataPin, LOW); //the money
+        digitalWrite(clockPin, HIGH);
+        digitalWrite(dataPin, LOW);
+      } else {
+        digitalWrite(clockPin, LOW);
+        digitalWrite(dataPin, HIGH); //the money
+        digitalWrite(clockPin, HIGH);
+        digitalWrite(dataPin, LOW);
+      }
+    }
+    
+    for (int i = 0; i < 8; i++) {
+      if (i == y) {
+        digitalWrite(clockPin, LOW);
+        digitalWrite(dataPin, LOW); //the money
+        digitalWrite(clockPin, HIGH);
+        digitalWrite(dataPin, LOW);
+      } else {
+        digitalWrite(clockPin, LOW);
+        digitalWrite(dataPin, HIGH); //the money
+        digitalWrite(clockPin, HIGH);
+        digitalWrite(dataPin, LOW);
+      }
+    }
+//  delay(450);
+//  digitalWrite(latchPin, LOW);
+//  digitalWrite(latchPin, HIGH);
+    for (int x = 0; x < 48; x++) {
+ if (x == 21) {
+        digitalWrite(clockPin, LOW);
+        digitalWrite(dataPin, LOW); //the money
+        digitalWrite(clockPin, HIGH);
+        digitalWrite(dataPin, LOW);
+      } else {
+        digitalWrite(clockPin, LOW);
+        digitalWrite(dataPin, HIGH); //the money
+        digitalWrite(clockPin, HIGH);
+        digitalWrite(dataPin, LOW);
+      }
     }
     
     for (int i = 0; i < 8; i++) {
---
+++ temp.js
@@ -0,0 +1,16 @@
+// const SerialPort = require('serialport');
+// const parsers = SerialPort.parsers;
+// const parser = new parsers.Readline({
+// delimiter: '\r\n'
+// });
+// // const arduinoPort = '/dev/ttyACM0';
+// const arduinoPort = '/dev/cu.usbmodemfa131';
+// const arduino = new SerialPort(arduinoPort, {
+// baudRate: 115200
+// });
+// arduino.pipe(parser);
+// arduino.on('open', function () {
+// console.log('Arduino connection');
+// parser.on('data', function (incomingData) {
+// });
+// });
