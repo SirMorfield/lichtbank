683407627209e7ba5fcd7e96e3278e2e8df4ec24
Joppe Koers <31915428+SirMorfield@users.noreply.github.com>
2018-11-10 13:57:18 +0100

    updates

---
+++ README.md
@@ -0,0 +1,2 @@
+main.ino is human readable, but slow
+fast.ino is the same but other, faster encoding of frames
--- temp.js
+++ temp.js
@@ -1,16 +1,32 @@
 // const SerialPort = require('serialport');
 // const parsers = SerialPort.parsers;
-// const parser = new parsers.Readline({
-// delimiter: '\r\n'
-// });
-// // const arduinoPort = '/dev/ttyACM0';
-// const arduinoPort = '/dev/cu.usbmodemfa131';
-// const arduino = new SerialPort(arduinoPort, {
-// baudRate: 115200
-// });
+// const parser = new parsers.Readline({ delimiter: '\r\n' });
+
+// // const arduinoPort = '/dev/ttyACM0'; //linux
+// const arduinoPort = '/dev/cu.usbmodemFA131'; //mac
+// const arduino = new SerialPort(arduinoPort, { baudRate: 115200 });
 // arduino.pipe(parser);
-// arduino.on('open', function () {
-// console.log('Arduino connection');
-// parser.on('data', function (incomingData) {
+
+// arduino.on('open', () => {
+//   console.log('Arduino connection');
+//   // parser.on('data', (incomingData) => {
+//   //   console.log(incomingData);
+//   // });
+//   setTimeout(function () {
+//     arduino.write("13")
+//   }, 1000);
 // });
-// });
+
+const Avrgirl = require('avrgirl-arduino');
+
+const avrgirl = new Avrgirl({
+  board: 'uno'
+});
+
+avrgirl.flash('./remove.ino.standard.hex', function (error) {
+  if (error) {
+    console.error(error);
+  } else {
+    console.info('done.');
+  }
+});
