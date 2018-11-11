d2ebae6e6ec206e611cade9fca5aef17a7a0d58c
SirMorfield <joppe.koers@gmail.com>
2018-11-11 14:12:19 +0100

    form rpi

--- app.js
+++ app.js
@@ -4,7 +4,6 @@ const app = express();
 const http = require('http').Server(app);
 const io = require('socket.io')(http);
 const { exec } = require('child_process')
-const arduinoPort = 'cu.usbmodemFA*';
 
 app.use(express.static(path.join(__dirname, 'public/')))
 
@@ -12,17 +11,19 @@ app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, './public/index.html'))
 });
 
-io.on('connection', function (socket) {
+io.on('connection', (socket) => {
   console.log('a user connected');
   socket.on('frame', (frame) => {
     console.log('uploading sketch');
-    let sketch = path.join(__dirname, './arduino/Blink/Blink.ino');
-    exec(`arduino --upload ${sketch} --board arduino:avr:uno --port  ${arduinoPort}`, (err, stdout, stderr) => {
+
+    let command = 'make upload clean'
+    let cwd = path.join(__dirname, 'sketch');
+    exec(command, { cwd: cwd }, (err, stdout, stderr) => {
       if (err) {
         console.error(err);
         return;
       }
-      console.log(`stdout: ${stdout}`);
+      // console.log(`stdout: ${stdout}`);
       console.log(`stderr: ${stderr}`);
     });
   })
