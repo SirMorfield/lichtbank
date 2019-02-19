53009651390cb6106a2691315c389554b67e149a
SirMorfield <joppe.koers@gmail.com>
2019-02-20 00:53:23 +0100

    ah hell yea it wurks

--- test.js
+++ test.js
@@ -1,4 +1,4 @@
 
-// clockPin   A0  SH_CP  GPIO2
-// latchPin   A1  ST_CP  GPIO3
-// data1Pin   A2  DS     GPIO4
+// clockPin   A0  SH_CP  GPIO2  bruin
+// latchPin   A1  ST_CP  GPIO3  paars
+// data1Pin   A2  DS     GPIO4  geel
