e473c874e35603894c66db385fedf01ddc5377bb
SirMorfield <joppe.koers@gmail.com>
2019-04-26 20:17:26 +0200

    flask is stom

--- app.js
+++ app.js
@@ -8,10 +8,8 @@ const { promisify } = require('util');
 const exec = promisify(require('child_process').exec);
 
 let jsonfile = require('jsonfile');
-const Xpix = 48;
-const Ypix = 48;
-const frameLength = 336
 
+const Ypix = 48
 
 app.use(express.static(path.join(__dirname, 'public/')))
 
@@ -19,80 +17,9 @@ app.get('/', (req, res) => {
   res.sendFile(path.join(__dirname, 'public/index.html'))
 });
 
-function serializeFrame(frame, log = false) {
-  let arr = [];
-  for (let column = 0; column < 8; column++) {
-    for (let panel = Ypix / 8 - 1; panel >= 0; panel--) {
-      if (panel % 2 == 1) {
-        for (let block = 0; block < 6; block++) {
-          for (let y = 0; y < 8; y++) {
-            let x = (7 - column) + (block * 8);
-            arr.push(frame[y + (panel * 8)][x]);
-          }
-        }
-      } else {
-        for (let block = 0; block < 6; block++) {
-          for (let y = 7; y >= 0; y--) {
-            let x = Xpix - 1 - (7 - column) - (block * 8);
-            arr.push(frame[y + (panel * 8)][x]);
-          }
-        }
-      }
-      for (let i = 0; i < 8; i++) {
-        if (i == column) arr.push(0);
-        else arr.push(1);
-      }
-    }
-  }
-
-  // this replaces the array of bits with an array of bytes
-  // so [1,0,0,1,1,1,0,1,1,0,0,1,1,1,0,1](16 bits) becomes [200,145](two bytes in base 10 format because js doesnt want to store bits)
-  let newArr = [];
-  for (let i = 0; i < arr.length; i += 8) {
-    let byte = '';
-    for (let bit = 0; bit < 8; bit++) {
-      byte += String(arr[bit + i]);
-    }
-    byte = parseInt(byte, 2);
-    newArr.push(byte);
-  }
-  arr = newArr;
-
-  if (log) console.log(JSON.stringify(arr));
-  return arr;
-}
-let writeToMatrix;
-let framePos = 0;
-let maxFramePos = 0;
-let serialized;
-
 io.on('connection', (socket) => {
   socket.on('frames', ({ frames, interval }) => {
-    maxFramePos = frames.length - 1;
-    let newSerialized = []
-
-    frames.forEach(frame => {
-      let strArr = serializeFrame(frame)
-      strArr = JSON.stringify(strArr)
-      strArr = strArr.substring(1, strArr.length - 1)
-      newSerialized.push(strArr)
-    });
-
-    serialized = newSerialized
-
-    if (writeToMatrix) clearInterval(writeToMatrix)
-    writeToMatrix = setInterval(() => {
-      exec(`python3 i2c.py ${serialized[framePos]} ${frameLength}`, { cwd: path.join(__dirname, '/server') })
-      framePos++
-      if (framePos > maxFramePos) framePos = 0
-    }, interval)
-
-    let currentFrames = []
-    writeToMatrix = setInterval(() => {
-
-
-    }, interval);
-
+    exec(`python3 i2c.py ${frames} ${interval} ${Ypix}`, { cwd: path.join(__dirname, '/server') })
   });
 
   // exec('make upload', { cwd: path.join(__dirname) }, (err, stdout, stderr) => {
