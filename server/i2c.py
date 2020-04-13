955d4db5ac16d339a5bd730a0d8ab997f90d0ad2
SirMorfield <joppe.koers@gmail.com>
2020-04-13 15:55:08 +0200

    completed python upload

--- server/i2c.py
+++ server/i2c.py
@@ -1,97 +1,60 @@
-# from BaseHTTPServer import BaseHTTPRequestHandler, HTTPServer
-# import json
-# from smbus import SMBus
-# import RPi.GPIO as GPIO
-
-# GPIO.setmode(GPIO.BCM)
-# GPIO.setwarnings(False)
-# GPIO.setup(21, GPIO.OUT)
-# bus = SMBus(1)
-
-# def upload(i2cBytes):
-# 	try:
-# 		GPIO.output(21, GPIO.HIGH)
-# 		sleep(0.0001)
-# 		GPIO.output(21, GPIO.LOW)
-# 		sleep(0.0001)
-
-# 		for i2cByte in i2cBytes:
-# 			bus.write_byte(0x04, i2cByte)
-# 			return 0
-# 	except:
-# 		print 'Upload failed'
-# 		return 1
-
-
-# class Server(BaseHTTPRequestHandler):
-# 	def _set_headers(self):
-# 		self.send_response(200)
-# 		self.send_header('Content-type', 'application/json')
-# 		self.end_headers()
-
-# 	def do_HEAD(self):
-# 		self._set_headers()
-
-# 	def log_message(self, format, *args):
-# 		return
-
-# 	def do_POST(self):
-# 		length = int(self.headers.getheader('content-length'))
-# 		message = json.loads(self.rfile.read(length))
-# 		print message
-
-# 		errorCounter = 0
-# 		while errorCounter < 10:
-# 			exitcode = upload(message)
-# 			if exitcode == 0:
-# 				return
-# 			else:
-# 				errorCounter += 1
-
-
-
-# def run(server_class=HTTPServer, handler_class=Server, port=8081):
-# 	server_address = ('', port)
-# 	httpd = server_class(server_address, handler_class)
-# 	httpd.serve_forever()
-
-# if __name__ == "__main__":
-# 	print 'running python'
-# 	run()
-
 from BaseHTTPServer import BaseHTTPRequestHandler, HTTPServer
+import json
+from smbus import SMBus
+import RPi.GPIO as GPIO
+from time import sleep
 
+GPIO.setmode(GPIO.BCM)
+GPIO.setwarnings(False)
+GPIO.setup(21, GPIO.OUT)
+bus = SMBus(1)
 
-class S(BaseHTTPRequestHandler):
-	def _set_headers(self):
-		self.send_response(200)
-		self.send_header("Content-type", "text/html")
-		self.end_headers()
+def upload(i2cBytes):
+	try:
+		GPIO.output(21, GPIO.HIGH)
+		sleep(0.0001)
+		GPIO.output(21, GPIO.LOW)
+		sleep(0.0001)
 
-	def do_GET(self):
-		self._set_headers()
-		print "get"
-		# self.wfile.write(self._html("hi!"))
+		for i2cByte in i2cBytes:
+			bus.write_byte(0x04, i2cByte)
+		return 0
+	except:
+		return 1
 
-	def do_HEAD(self):
-		self._set_headers()
+class Server(BaseHTTPRequestHandler):
+  def _set_headers(self):
+	self.send_response(200)
+	self.send_header('Content-type', 'application/json')
+	self.end_headers()
 
-	def do_POST(self):
-		# Doesn't do anything with posted data
-		length = int(self.headers.getheader('content-length'))
-		message = json.loads(self.rfile.read(length))
-		print message
-		self._set_headers()
-		# self.wfile.write(self._html("POST!"))
+  def log_message(self, format, *args):
+	return
 
+  def do_POST(self):
+	length = int(self.headers.getheader('content-length'))
+	message = json.loads(self.rfile.read(length))
 
-def run(server_class=HTTPServer, handler_class=S, addr="localhost", port=8081):
-	server_address = (addr, port)
-	httpd = server_class(server_address, handler_class)
+	errorCounter = 0
+	while True:
+	  exitcode = upload(message)
 
-	print "Starting httpd"
-	httpd.serve_forever()
+	  if exitcode == 0:
+		break
 
+	  elif exitcode == 1:
+		errorCounter += 1
+		if errorCounter > 10:
+		  print "upload failed 10 times PANIC NOW"
+		  break
+	self._set_headers()
+
+def run(server_class=HTTPServer, handler_class=Server, port=8081):
+  server_address = ('', port)
+  httpd = server_class(server_address, handler_class)
+
+  httpd.serve_forever()
 
 if __name__ == "__main__":
-	run()
+  print 'running python'
+  run()
--- server/writeToPython.js
+++ server/writeToPython.js
@@ -1,52 +1,19 @@
-// const spawn = require('child_process').spawn
-// const python = spawn('python', ['-u', 'i2c.py'], { cwd: __dirname })
-// python.stdout.on('data', (data) => { if (data.length > 0) console.log(data.toString()) })
-// python.stderr.on('data', (data) => { if (data.length > 0) console.error(data.toString()) })
-// process.on('exit', () => python.kill())
+const spawn = require('child_process').spawn
+const python = spawn('python', ['-u', 'i2c.py'], { cwd: __dirname })
+python.stdout.on('data', (data) => { if (data.toString().length > 0) console.log(data.toString()) })
+python.stderr.on('data', (data) => { if (data.toString().length > 0) console.error(data.toString()) })
+process.on('exit', () => python.kill())
 
-// const request = require('request-promise-native')
-// async function writeToPython(bytes) {
-// 	const options = {
-// 		url: 'http://localhost:8081',
-// 		method: 'POST',
-// 		headers: { 'Content-Type': 'application/json' },
-// 		body: JSON.stringify(bytes)
-// 	}
-// 	const { err, response, body } = await request(options)
-// 	if (err) console.error(err)
-// 	console.log('send')
-// }
-
-
-const http = require('http')
+const request = require('request-promise-native')
 async function writeToPython(bytes) {
-
-	const data = JSON.stringify(bytes)
 	const options = {
-		hostname: 'http://localhost',
-		port: 8081,
-		path: '/',
+		url: 'http://localhost:8081',
 		method: 'POST',
-		headers: {
-			'Content-Type': 'application/json',
-			'Content-Length': data.length
-		}
+		headers: { 'Content-Type': 'application/json' },
+		body: JSON.stringify(bytes)
 	}
-
-	const req = http.request(options, (res) => {
-		console.log(`statusCode: ${res.statusCode}`)
-
-		res.on('data', (d) => {
-			process.stdout.write(d)
-		})
-	})
-
-	req.on('error', (error) => {
-		console.error(error)
-	})
-
-	req.write(data)
-	req.end()
+	const { err, response, body } = await request(options)
+	if (err) console.error(err)
 }
-// module.exports = writeToPython
-writeToPython([1, 2, 3])
+
+module.exports = writeToPython
